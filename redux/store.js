6ab38c9ad552f13d5299783ef414981dec9d15bc
Bonaventure Chukwudi <bonarhyme@gmail.com>
2022-06-27 16:14:47 +0100

    Added redux store

---
+++ redux/store.js
@@ -0,0 +1,41 @@
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// import {
+//   userRegisterReducer,
+//   userLoginReducer,
+// } from "./reducers/userReducers";
+
+const reducer = combineReducers({
+  // registerUser: userRegisterReducer,
+});
+
+// Local storage matters
+let userInfoFromStorage;
+if (typeof window !== "undefined") {
+  userInfoFromStorage = localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null;
+}
+
+// initial state
+const initialState = {
+  userLogin: {
+    userInfo: userInfoFromStorage,
+  },
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
